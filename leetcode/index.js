0f85b77eaba858ada26332ce3add98dc8cecd173
apprehen <3514392356@qq.com>
2023-10-01 13:15:05 +0800

    feat(all): new add leetcode and react

---
+++ leetcode/index.js
@@ -0,0 +1,15 @@
+/**
+ * 最长回文子串
+ * @param {string} s
+ * @return {string}
+ * */
+// 满足条件：1.最长 2.回文
+var longestPalindrome = function(s) {
+  const len = s.length
+  if (len < 2) return s
+  // 滑动窗口
+  let left = 0, right = 0
+  for (let i = 0; i < len; i++) { 
+    
+  }
+};
